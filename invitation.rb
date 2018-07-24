1a56ddf3ef20016025929fef25424968272cb584
hanam0822 <hanam0822@gmail.com>
2018-07-24 13:23:49 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,5 +1,8 @@
 guest_name = Tom
 party_name = Ultimate Birthday Bash 
-date  = August 22 
+date  = the third of August  
 time = 3pm
 host_name = Hana
+
+puts "Dear #{guest_name}, "
+puts "You are invited to #{party_name}" on 
